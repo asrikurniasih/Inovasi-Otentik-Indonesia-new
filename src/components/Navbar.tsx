88c943da7020de9fcd55301211c8784e281ffca9
asrikurniasih <asri.kurniasih@gmail.com>
2025-08-08 23:03:00 +0700

    fixing new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -146,7 +146,7 @@ export default function ComplexNavbar() {
         >
           <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
+          </svg> 
         </button>
         
         {/* <a 
